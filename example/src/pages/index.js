ee4fbb783b1d0ec72712bd758fb0aa9504d8f591
nilo <2960831538@qq.com>
2019-12-02 00:56:24 +0800

    almost finish

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,18 +1,102 @@
 import React from 'react'
 import { Collapse } from '../../../src/index.js'
 
+
+const OtherNode = (props) => {
+    return (<svg t="1575213818852" style={{curser: 'pointer'}} onClick={ event => {event.stopPropagation();}} className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="10743" width="20" height="20"><path d="M510.55151 231.092215c-168.745239 0-449.311239 134.080741-449.311239 299.542191 0 165.42154 280.564977 299.520701 449.311239 299.520701S959.861726 696.055946 959.861726 530.634406c0-165.461449-280.564977-299.54219-449.310216-299.542191z m0 539.240411c-140.320862 0-389.691373-107.318214-389.691373-239.69822 0-132.3841 249.371534-239.718687 389.691373-239.718687 140.319839 0 389.691373 107.334587 389.691373 239.718687 0 132.380006-249.371534 239.698221-389.691373 239.69822z" fill="#020202" p-id="10744"></path><path d="M510.55151 350.886579c-99.268888 0-179.707918 80.478939-179.707918 179.747827 0 99.249445 80.43903 179.708941 179.707918 179.708941 99.269911 0 179.708941-80.459496 179.708941-179.708941 0-99.269911-80.43903-179.747827-179.708941-179.747827z m0 299.559586c-66.191538 0-119.832226-53.658084-119.832226-119.811759 0-66.192561 53.639664-119.833249 119.832226-119.833249 66.154699 0 119.79334 53.640688 119.79334 119.833249 0 66.154699-53.638641 119.81176-119.79334 119.811759z" fill="#020202" p-id="10745"></path></svg>)
+}
+
+const CustomIcon = (props) => {
+    return <svg t="1575218747174" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11781" width="40" height="40"><path d="M18.5 0v1024" fill="" p-id="11782"></path><path d="M34.5 0v1024" fill="" p-id="11783"></path><path d="M50.5 0v1024" fill="" p-id="11784"></path><path d="M66.5 0v1024" fill="" p-id="11785"></path><path d="M82.5 0v1024" fill="" p-id="11786"></path><path d="M98.5 0v1024" fill="" p-id="11787"></path><path d="M114.5 0v1024" fill="" p-id="11788"></path><path d="M130.5 0v1024" fill="" p-id="11789"></path><path d="M146.5 0v1024" fill="" p-id="11790"></path><path d="M162.5 0v1024" fill="" p-id="11791"></path><path d="M178.5 0v1024" fill="" p-id="11792"></path><path d="M194.5 0v1024" fill="" p-id="11793"></path><path d="M210.5 0v1024" fill="" p-id="11794"></path><path d="M226.5 0v1024" fill="" p-id="11795"></path><path d="M242.5 0v1024" fill="" p-id="11796"></path><path d="M258.5 0v1024" fill="" p-id="11797"></path><path d="M274.5 0v1024" fill="" p-id="11798"></path><path d="M290.5 0v1024" fill="" p-id="11799"></path><path d="M306.5 0v1024" fill="" p-id="11800"></path><path d="M322.5 0v1024" fill="" p-id="11801"></path><path d="M338.5 0v1024" fill="" p-id="11802"></path><path d="M354.5 0v1024" fill="" p-id="11803"></path><path d="M370.5 0v1024" fill="" p-id="11804"></path><path d="M386.5 0v1024" fill="" p-id="11805"></path><path d="M402.5 0v1024" fill="" p-id="11806"></path><path d="M418.5 0v1024" fill="" p-id="11807"></path><path d="M434.5 0v1024" fill="" p-id="11808"></path><path d="M450.5 0v1024" fill="" p-id="11809"></path><path d="M466.5 0v1024" fill="" p-id="11810"></path><path d="M482.5 0v1024" fill="" p-id="11811"></path><path d="M498.5 0v1024" fill="" p-id="11812"></path><path d="M514.5 0v1024" fill="" p-id="11813"></path><path d="M530.5 0v1024" fill="" p-id="11814"></path><path d="M546.5 0v1024" fill="" p-id="11815"></path><path d="M562.5 0v1024" fill="" p-id="11816"></path><path d="M578.5 0v1024" fill="" p-id="11817"></path><path d="M594.5 0v1024" fill="" p-id="11818"></path><path d="M610.5 0v1024" fill="" p-id="11819"></path><path d="M626.5 0v1024" fill="" p-id="11820"></path><path d="M642.5 0v1024" fill="" p-id="11821"></path><path d="M658.5 0v1024" fill="" p-id="11822"></path><path d="M674.5 0v1024" fill="" p-id="11823"></path><path d="M690.5 0v1024" fill="" p-id="11824"></path><path d="M706.5 0v1024" fill="" p-id="11825"></path><path d="M722.5 0v1024" fill="" p-id="11826"></path><path d="M738.5 0v1024" fill="" p-id="11827"></path><path d="M754.5 0v1024" fill="" p-id="11828"></path><path d="M770.5 0v1024" fill="" p-id="11829"></path><path d="M786.5 0v1024" fill="" p-id="11830"></path><path d="M802.5 0v1024" fill="" p-id="11831"></path><path d="M818.5 0v1024" fill="" p-id="11832"></path><path d="M834.5 0v1024" fill="" p-id="11833"></path><path d="M850.5 0v1024" fill="" p-id="11834"></path><path d="M866.5 0v1024" fill="" p-id="11835"></path><path d="M882.5 0v1024" fill="" p-id="11836"></path><path d="M898.5 0v1024" fill="" p-id="11837"></path><path d="M914.5 0v1024" fill="" p-id="11838"></path><path d="M930.5 0v1024" fill="" p-id="11839"></path><path d="M946.5 0v1024" fill="" p-id="11840"></path><path d="M962.5 0v1024" fill="" p-id="11841"></path><path d="M978.5 0v1024" fill="" p-id="11842"></path><path d="M994.5 0v1024" fill="" p-id="11843"></path><path d="M1010.5 0v1024M2.5 16h1019" fill="" p-id="11844"></path><path d="M2.5 32h1019" fill="" p-id="11845"></path><path d="M2.5 48h1019" fill="" p-id="11846"></path><path d="M2.5 64h1019" fill="" p-id="11847"></path><path d="M2.5 80h1019" fill="" p-id="11848"></path><path d="M2.5 96h1019" fill="" p-id="11849"></path><path d="M2.5 112h1019" fill="" p-id="11850"></path><path d="M2.5 128h1019" fill="" p-id="11851"></path><path d="M2.5 144h1019" fill="" p-id="11852"></path><path d="M2.5 160h1019" fill="" p-id="11853"></path><path d="M2.5 176h1019" fill="" p-id="11854"></path><path d="M2.5 192h1019" fill="" p-id="11855"></path><path d="M2.5 208h1019" fill="" p-id="11856"></path><path d="M2.5 224h1019" fill="" p-id="11857"></path><path d="M2.5 240h1019" fill="" p-id="11858"></path><path d="M2.5 256h1019" fill="" p-id="11859"></path><path d="M2.5 272h1019" fill="" p-id="11860"></path><path d="M2.5 288h1019" fill="" p-id="11861"></path><path d="M2.5 304h1019" fill="" p-id="11862"></path><path d="M2.5 320h1019" fill="" p-id="11863"></path><path d="M2.5 336h1019" fill="" p-id="11864"></path><path d="M2.5 352h1019" fill="" p-id="11865"></path><path d="M2.5 368h1019" fill="" p-id="11866"></path><path d="M2.5 384h1019" fill="" p-id="11867"></path><path d="M2.5 400h1019" fill="" p-id="11868"></path><path d="M2.5 416h1019" fill="" p-id="11869"></path><path d="M2.5 432h1019" fill="" p-id="11870"></path><path d="M2.5 448h1019" fill="" p-id="11871"></path><path d="M2.5 464h1019" fill="" p-id="11872"></path><path d="M2.5 480h1019" fill="" p-id="11873"></path><path d="M2.5 496h1019" fill="" p-id="11874"></path><path d="M2.5 512h1019" fill="" p-id="11875"></path><path d="M2.5 528h1019" fill="" p-id="11876"></path><path d="M2.5 544h1019" fill="" p-id="11877"></path><path d="M2.5 560h1019" fill="" p-id="11878"></path><path d="M2.5 576h1019" fill="" p-id="11879"></path><path d="M2.5 592h1019" fill="" p-id="11880"></path><path d="M2.5 608h1019" fill="" p-id="11881"></path><path d="M2.5 624h1019" fill="" p-id="11882"></path><path d="M2.5 640h1019" fill="" p-id="11883"></path><path d="M2.5 656h1019" fill="" p-id="11884"></path><path d="M2.5 672h1019" fill="" p-id="11885"></path><path d="M2.5 688h1019" fill="" p-id="11886"></path><path d="M2.5 704h1019" fill="" p-id="11887"></path><path d="M2.5 720h1019" fill="" p-id="11888"></path><path d="M2.5 736h1019" fill="" p-id="11889"></path><path d="M2.5 752h1019" fill="" p-id="11890"></path><path d="M2.5 768h1019" fill="" p-id="11891"></path><path d="M2.5 784h1019" fill="" p-id="11892"></path><path d="M2.5 800h1019" fill="" p-id="11893"></path><path d="M2.5 816h1019" fill="" p-id="11894"></path><path d="M2.5 832h1019" fill="" p-id="11895"></path><path d="M2.5 848h1019" fill="" p-id="11896"></path><path d="M2.5 864h1019" fill="" p-id="11897"></path><path d="M2.5 880h1019" fill="" p-id="11898"></path><path d="M2.5 896h1019" fill="" p-id="11899"></path><path d="M2.5 912h1019" fill="" p-id="11900"></path><path d="M2.5 928h1019" fill="" p-id="11901"></path><path d="M2.5 944h1019" fill="" p-id="11902"></path><path d="M2.5 960h1019" fill="" p-id="11903"></path><path d="M2.5 976h1019" fill="" p-id="11904"></path><path d="M2.5 992h1019" fill="" p-id="11905"></path><path d="M2.5 1008h1019" fill="" p-id="11906"></path><path d="M367.368 723.577V300.425c0-11.952 15.543-20.321 26.297-10.76l253.41 206.797c9.557 7.171 9.557 22.713 0 29.877l-253.41 209.186c-10.76 8.371-26.297 1.197-26.297-11.952z" fill="#666666" p-id="11907"></path></svg>
+}
+
+const customPanelStyle = {
+    background: '#f7f7f7',
+    borderRadius: 4,
+    marginBottom: 24,
+    border: 0,
+    overflow: 'hidden',
+};
+
 export default class extends React.Component {
     render() {
         const { Panel } = Collapse
         return (
             <div>
-                折叠面板
+                <p>折叠面板</p>
+                <p>普通模式</p>
                 <Collapse>
                     <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
     as a welcome guest in many households across the world.</Panel>
                     <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
     as a welcome guest in many households across the world.</Panel>
                     <Panel header="This is panel header 1" disabled={true}> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>手风琴模式</p>
+                <Collapse accordion={true}>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>嵌套模式</p>
+                <Collapse>
+                    <Panel header="This is panel header 1">
+                        <Collapse>
+                            <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+            as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                        </Collapse>
+                    </Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>简洁模式</p>
+                <Collapse bordered={false}>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.
+                    </Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>隐藏箭头</p>
+                <Collapse>
+                    <Panel showArrow={false} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.
+                    </Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>额外节点</p>
+                <Collapse>
+                    <Panel showArrow={false} extra={<OtherNode />} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel extra={<OtherNode />} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel extra={<OtherNode />} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                </Collapse>
+                <p>自定义面板</p>
+                <Collapse expandIcon={<CustomIcon />}>
+                    <Panel style={customPanelStyle} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel style={customPanelStyle} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+    as a welcome guest in many households across the world.</Panel>
+                    <Panel style={customPanelStyle} header="This is panel header 1"> A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
     as a welcome guest in many households across the world.</Panel>
                 </Collapse>
             </div>
--- src/collapse/index.js
+++ src/collapse/index.js
@@ -1,36 +1,74 @@
 import React from 'react'
 
-const Panel = (props) => {
+const ArrowIcon = (props) => {
+    const { selected, position, expandIcon } = props
+    return expandIcon ? <div className={`icon arrow-icon${selected ? ' arrow-icon-selected' : ''} icon-${position}`}>{expandIcon}</div> : <svg className={`icon arrow-icon${selected ? ' arrow-icon-selected' : ''} icon-${position}`} t="1575195983575" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9932" width="14" height="14"><path d="M300.6 895.7c9.6 0 19.3-3.2 27.4-9.6L751.8 545.2c10.3-8.3 16.3-20.8 16.3-34.1 0-13.2-6-25.8-16.3-34L329.8 138c-18.8-15.1-46.3-12.1-61.4 6.7-15.1 18.8-12.1 46.3 6.7 61.4l379.6 305.1L273.2 818c-18.8 15.1-21.8 42.6-6.7 61.4 8.7 10.7 21.3 16.3 34.1 16.3z" fill="#000000" opacity=".65" p-id="9933"></path></svg>
+}
+
+class Panel extends React.Component {
     
-    const defaultProps = {
+    defaultPanelProps = {
         disabled: false,
         header: '',
         key: '',
         showArrow: true,
         extra: null
     }
-    const { children, disabled, header, key, showArrow, extra } = props
-    console.log(disabled)
-    return (
-    <div className="collapse-panel">
-        <div className={`${disabled ? 'disabled ' : ''}collapse-panel-head`}>{header}</div>
-        <div className="collapse-panel-content">
-            {children}
-        </div>
-    </div>
-    )
-}
 
-let defaultProps = {
-    accordion: false,
-    defaultActiveKey: undefined,
-    onChange: () => {},
-    bordered: true,
-    expandIcon: undefined,
-    expandIconPosition	: 'left'
+    getValid (value, defaultValue) {
+        if (value !== undefined) return value
+        return defaultValue
+    }
+
+    currentProps() {
+        const { disabled, header, key, showArrow, extra } = this.props
+        const defaultProps = this.defaultPanelProps
+        let tempProps = { ...this.defaultPanelProps}
+        tempProps.disabled = this.getValid(disabled, defaultProps.disabled)
+        tempProps.header = this.getValid(header, defaultProps.header)
+        tempProps.key = this.getValid(key, defaultProps.key)
+        tempProps.showArrow = this.getValid(showArrow, defaultProps.showArrow)
+        tempProps.extra = this.getValid(extra, defaultProps.extra)
+        return tempProps
+    }
+
+    render() {
+        const { children, changeActive, index, activeValue, expandIconPosition, bordered, expandIcon, style } = this.props
+        const { disabled, header, key, showArrow, extra } = this.currentProps()
+        const selected = activeValue.find(value => value === index) !== undefined
+        const tempStyle = (!bordered && selected) ? {borderBottom: 'none'} : null
+        let classArray = ['collapse-panel-head']
+        bordered && classArray.push('panel-head-bordered')
+        disabled && classArray.push('disabled')
+        showArrow && classArray.push(`head-with-icon-${expandIconPosition}`)
+        bordered && selected && !style && classArray.push('panel-head-bordered-actived')
+        console.log(showArrow)
+        return (
+            <div className={`collapse-panel${bordered ? ' collapse-panel-bordered' : ''}`} style={style}>
+                <div className={classArray.join(' ')} style={tempStyle} onClick={() => {!disabled && changeActive(index)}}>
+                    {showArrow && <ArrowIcon selected={selected} position={expandIconPosition} expandIcon={expandIcon}/>}
+                    {header}
+                    {extra}
+                </div>
+                <div className={`collapse-panel-content${selected ? '' : ' fold-content'}`}>
+                    {children}
+                </div>
+            </div>
+        )
+    }
+    
 }
 
 class Collapse extends React.Component {
+
+    defaultCollapseProps = {
+        accordion: false,
+        defaultActiveKey: undefined,
+        onChange: () => {},
+        bordered: true,
+        expandIcon: undefined,
+        expandIconPosition	: 'left'
+    }
 
     constructor (props) {
         super(props)
@@ -46,7 +84,8 @@ class Collapse extends React.Component {
 
     currentProps () {
         const { accordion, onChange, defaultActiveKey, bordered, expandIcon, expandIconPosition } = this.props
-        let tempProps = { ...this.defaultProps}
+        let tempProps = { ...this.defaultCollapseProps}
+        const defaultProps = this.defaultCollapseProps
         tempProps.accordion = this.getValid(accordion, defaultProps.accordion)
         tempProps.onChange = this.getValid(onChange, defaultProps.onChange)
         tempProps.defaultActiveKey = this.getValid(defaultActiveKey, defaultProps.defaultActiveKey)
@@ -56,12 +95,29 @@ class Collapse extends React.Component {
         return tempProps
     }
 
+    changeActive(res) {
+        const { accordion } = this.props
+        let { activeValue } = this.state
+        if (activeValue.find(value => value === res) !== undefined){
+            activeValue.splice(activeValue.findIndex(value => value === res),1)
+        }else{
+            if (accordion) {
+                activeValue = []
+            }
+            activeValue.push(res)
+        }
+        this.setState({
+            activeValue: activeValue
+        })
+    }
+
     render () {
         const { accordion, onChange, defaultActiveKey, bordered, expandIcon, expandIconPosition } = this.currentProps();
-        const { children } = this.props
+        const { children, style } = this.props
+        const { activeValue } = this.state 
         return (
-        <div className={`react-collapse-background${bordered ? ' bordered' : ''}`}>
-            {children}
+        <div className='react-collapse-background' style={style}>
+            {React.Children.map(children, (child, index) => React.cloneElement(child, {activeValue: activeValue, index: index, changeActive: this.changeActive.bind(this), expandIconPosition: expandIconPosition, bordered: bordered, expandIcon: expandIcon}))}
         </div>)
     }
 }
